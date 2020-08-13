f261732c0e3227222dfafaa30b0d07118cd045d7
Bartek Łukawski <bwmn.peter@gmail.com>
2020-08-13 12:44:05 +0200

    Fix rename leftovers (CommPickDetectionRequest)
    Follows up ad113541bcdc4d201dccfbeebb8b3c4d46c1fb88.

--- CommObjectRecognitionObjects/coordination/src/PickDetectionCoordinationService/PickDetectionCoordinationServicePickqueryQueryHandler.hh
+++ CommObjectRecognitionObjects/coordination/src/PickDetectionCoordinationService/PickDetectionCoordinationServicePickqueryQueryHandler.hh
@@ -1,7 +1,7 @@
 #ifndef _PICKDETECTIONCOORDINATIONSERVICEPICKQUERYQUERYHANDLER_HH_
 #define _PICKDETECTIONCOORDINATIONSERVICEPICKQUERYQUERYHANDLER_HH_
 
-#include "CommObjectRecognitionObjects/PickDetectionRequest.hh"
+#include "CommObjectRecognitionObjects/CommPickDetectionRequest.hh"
 #include "CommManipulatorObjects/CommGrasp.hh"
 #include <string>
 
@@ -13,7 +13,7 @@ public:
 
 	}
 
-	CommObjectRecognitionObjects::PickDetectionRequest handleRequest(const std::string& inString);
+	CommObjectRecognitionObjects::CommPickDetectionRequest handleRequest(const std::string& inString);
 
 	std::string handleAnswer(const CommManipulatorObjects::CommGrasp& answer);
 };
